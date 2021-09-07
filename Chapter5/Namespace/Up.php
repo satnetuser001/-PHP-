fe50c68880fb1aa810ba0132b1f8aa28af9321a6
satnetuser001 <76094506+satnetuser001@users.noreply.github.com>
2021-09-07 10:36:05 +0300

    Update Up.php

--- Chapter5/Namespace/Up.php
+++ Chapter5/Namespace/Up.php
@@ -1,1 +1,11 @@
+<?php
+//Подразумевается, что имена функций будут одинаковые, но для наглядности - разные.
+require_once('down/down.php');
+$upVar = "request from up";
 
+function upf($e = "non arg"){
+	echo "echo from up dir, $e\n";
+}
+upf($upVar);
+down\downf($upVar);
+?>
