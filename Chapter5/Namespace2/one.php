3f260e73f151681f1112c4254d741912ce5c7ef4
satnetuser001 <76094506+satnetuser001@users.noreply.github.com>
2022-01-20 12:16:06 +0200

    Create one.php

---
+++ Chapter5/Namespace2/one.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Если в файле объявленно namespace(см. three.php), то к коду
+этого файла можно обратится (из one.php) только через
+пространство имен или псевдоним присвоенный namespace.
+Плюс namespace - возможность использования одинаковых имен
+в разных файлах(подключенных вместе). 
+*/
+
+require_once 'folderTwo/two.php';
+require_once 'folderThree/three.php';
+
+use folderThree\three;
+use folderThree\three as fileThree;
+
+function oneFunc(){
+	echo "one\n";
+}
+
+oneFunc();
+twoFunc();
+folderThree\three\threeFunc();
+three\threeFunc();
+fileThree\threeFunc();
+three\oneFunc();
+
+?>
